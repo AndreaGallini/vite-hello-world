875b78a50abf56fb84e08baf5e6289c9f617be30
AndreaGallini <andrea.gallini01@libero.it>
2022-11-09 15:13:06 +0100

    add h1 e img

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
+<template>
+  <div>
+    <h1>La mia prima app vite!!!</h1>
 
+    <img src="../public/img/cane.jpeg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
