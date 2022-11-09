5d36af10434afc8f9f14389b0deafdafa1756b0a
AndreaGallini <andrea.gallini01@libero.it>
2022-11-09 15:27:15 +0100

    add jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <AppTitle />
+    <JumboComponent />
   </div>
 </template>
 
 <script>
+import JumboComponent from "./components/JumboComponent.vue";
 import AppTitle from "./components/AppTitle.vue";
+
 export default {
   components: {
     AppTitle,
+    JumboComponent,
   },
 };
 </script>
